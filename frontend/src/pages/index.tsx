20a5bb5d02dc7e1d60a5e5298eab227ad4f4746b
Rodrigo de França <howdrigo.fr@gmail.com>
2022-02-04 21:56:47 -0300

    feat: implemented homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,18 @@
-import HomeTemplate from 'templates/Home'
+import { GetServerSideProps } from 'next'
 
-export default function Home() {
-  return <HomeTemplate />
+import api from 'services/api'
+
+import HomeTemplate, { HomeTemplateProps } from 'templates/Home'
+// import HomeTemplateMock from 'templates/Home/mock'
+
+export default function HomePage(props: HomeTemplateProps) {
+  return <HomeTemplate {...props} />
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await api.get<HomeTemplateProps>('/products')
+
+  return {
+    props: data
+  }
 }
