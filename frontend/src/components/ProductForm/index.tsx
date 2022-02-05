f7e0ca9ad024afd7369273b0d649f591837b817c
Rodrigo de França <howdrigo.fr@gmail.com>
2022-02-05 18:05:42 -0300

    feat: implemented ribbon buttons

--- frontend/src/components/ProductForm/index.tsx
+++ frontend/src/components/ProductForm/index.tsx
@@ -2,19 +2,18 @@ import { ChangeEvent } from 'react'
 import Input from 'components/Input'
 import UploadImage from 'components/UploadImage'
 import * as Styled from './styles'
+import { FaCheck, FaEraser } from 'react-icons/fa'
 
 export type ProductFormProps = {
   imageChange: (e: ChangeEvent<HTMLInputElement>) => void
   handleResetForm: () => void
   previewImage?: string
-  submitButtonLabel: string
 }
 
 const ProductForm = ({
   imageChange,
   handleResetForm,
-  previewImage,
-  submitButtonLabel
+  previewImage
 }: ProductFormProps) => (
   <Styled.Wrapper>
     <UploadImage
@@ -25,13 +24,15 @@ const ProductForm = ({
     <Styled.Content>
       <Input name="name" label="Nome do Produto" />
       <Input name="price" label="Preço" />
-      <Styled.ButtonsContent>
-        <button type="reset" onClick={handleResetForm}>
-          limpar
-        </button>
-        <button type="submit">{submitButtonLabel}</button>
-      </Styled.ButtonsContent>
     </Styled.Content>
+    <Styled.ButtonsContent>
+      <button type="reset" onClick={handleResetForm}>
+        <FaEraser size={20} />
+      </button>
+      <button type="submit">
+        <FaCheck size={20} />
+      </button>
+    </Styled.ButtonsContent>
   </Styled.Wrapper>
 )
 
--- frontend/src/components/ProductForm/styles.ts
+++ frontend/src/components/ProductForm/styles.ts
@@ -5,30 +5,44 @@ export const Wrapper = styled.div`
   flex-direction: column;
   flex: 1;
   width: 40rem;
+  position: relative;
 `
 
 export const Content = styled.div`
-  padding-top: 1.5rem;
+  padding: 1.5rem;
   border: 2px solid #4a63e5;
   border-top: none;
   border-radius: 0 0 2rem 2rem;
-  padding-left: 1rem;
-  padding-right: 1rem;
 `
 
 export const ButtonsContent = styled.div`
+  position: absolute;
+  top: 0;
+  right: -4rem;
+  z-index: 5;
   display: flex;
+  width: 6rem;
+  height: 9rem;
+  overflow: hidden;
+  background-color: #4a63e5;
+  border-radius: 0 2rem 2rem 0;
+  flex-direction: column;
+  align-items: flex-end;
   justify-content: space-around;
-  padding: 2rem;
 
   button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     outline: none;
     border: none;
-    padding: 2rem;
-    cursor: pointer;
+    background: transparent;
     color: #fff;
-    background-color: #4a63e5;
-    border-radius: 1rem;
-    width: 30%;
+    padding: 1rem;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 `
