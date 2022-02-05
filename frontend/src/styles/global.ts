c3eab50e66cb5b2df460b2d5aa8ac43400f09f33
Rodrigo de França <howdrigo.fr@gmail.com>
2022-02-04 21:56:13 -0300

    add font-family and background

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -16,7 +16,8 @@ const GlobalStyles = createGlobalStyle`
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: #FEFEFE;
   }
 `
 
