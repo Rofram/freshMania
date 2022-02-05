9d73e1aed7de8c3f31a76744a1478975cf416cd2
Rodrigo de França <howdrigo.fr@gmail.com>
2022-02-05 18:07:06 -0300

    feat: implemented ribbon buttons and function handleDelete product

--- frontend/src/components/ProductBox/index.tsx
+++ frontend/src/components/ProductBox/index.tsx
@@ -3,6 +3,10 @@ import * as Styled from './styles'
 import Image from 'next/image'
 import getImageUrl from 'utils/getImageUrl'
 import formatPrice from 'utils/format-price'
+import { FaTrash, FaEdit } from 'react-icons/fa'
+import { useRef } from 'react'
+import api from 'services/api'
+import { toast } from 'react-toastify'
 
 export type ProductBoxProps = {
   id: string
@@ -12,15 +16,32 @@ export type ProductBoxProps = {
 }
 
 const ProductBox = ({ id, img, name, price }: ProductBoxProps) => {
+  const productBoxRef = useRef<HTMLDivElement>(null)
+
   function handleEdit() {
     console.log('')
   }
 
-  function handleDelete(productId: string) {
-    console.log('')
+  async function handleDelete(productId: string) {
+    const response = await api.delete('/products', {
+      params: {
+        id: productId
+      }
+    })
+
+    if (response.status === 200) {
+      productBoxRef.current?.remove()
+      toast('produto deletado com sucesso!', {
+        type: 'success'
+      })
+    } else {
+      toast('não foi possível deletar o produto, tente novamente mais tarde.', {
+        type: 'error'
+      })
+    }
   }
   return (
-    <Styled.Wrapper>
+    <Styled.Wrapper ref={productBoxRef}>
       <Styled.ImageBox>
         <Image
           src={getImageUrl(img)}
@@ -33,6 +54,14 @@ const ProductBox = ({ id, img, name, price }: ProductBoxProps) => {
         <h1>{name}</h1>
         <Styled.Price>{formatPrice(price)}</Styled.Price>
       </Styled.Content>
+      <Styled.ProductOptions>
+        <button onClick={() => handleDelete(id)}>
+          <FaTrash size={20} />
+        </button>
+        <button>
+          <FaEdit size={20} />
+        </button>
+      </Styled.ProductOptions>
     </Styled.Wrapper>
   )
 }
--- frontend/src/components/ProductBox/styles.ts
+++ frontend/src/components/ProductBox/styles.ts
@@ -1,10 +1,33 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+const popup = keyframes`
+  from {
+    opacity: 0;
+    transform: scale(0.5);
+  }
+  to {
+    opacity: 1;
+    transform: scale(1);
+  }
+`
 
 export const Wrapper = styled.div`
   max-width: 25rem;
   border: 2px solid #4a63e5;
+  background-color: #4a63e5;
   border-radius: 2rem;
   position: relative;
+  z-index: 10;
+  animation: ${popup} 0.3s ease-in-out;
+`
+
+const placeholderShimmer = keyframes`
+  from {
+    background-position: -40rem 0;
+  }
+  to {
+    background-position: 40rem 0;
+  }
 `
 
 export const ImageBox = styled.div`
@@ -13,6 +36,16 @@ export const ImageBox = styled.div`
   width: 24rem;
   height: 20rem;
   border-bottom: 2px solid #4a63e5;
+  border-radius: 2rem 2rem 0 0;
+  z-index: 10;
+  background-image: linear-gradient(
+    to right,
+    #f6f7f8 0%,
+    #edeef1 20%,
+    #f6f7f8 40%,
+    #f6f7f8 100%
+  );
+  animation: ${placeholderShimmer} 1s linear infinite forwards;
 
   img {
     border-radius: 2rem 2rem 0 0;
@@ -25,10 +58,43 @@ export const Content = styled.div`
   align-items: flex-end;
   height: 100%;
   margin: 1.6rem;
+  color: #f1f1f1;
 `
 
 export const Price = styled.div`
   display: inline-block;
   font-weight: bold;
   height: 1rem;
+`
+
+export const ProductOptions = styled.div`
+  position: absolute;
+  top: 0;
+  right: -4rem;
+  z-index: 5;
+  display: flex;
+  width: 5rem;
+  height: 9rem;
+  overflow: hidden;
+  background-color: #4a63e5;
+  border-radius: 0 2rem 2rem 0;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: space-around;
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    outline: none;
+    border: none;
+    background: transparent;
+    color: #fff;
+    padding: 1rem;
+    cursor: pointer;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
+  }
 `
