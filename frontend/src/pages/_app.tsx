bb0f62cc1d86d5476505b6f0bc9c72eb7e63f4c5
Rodrigo de França <howdrigo.fr@gmail.com>
2022-02-04 21:58:28 -0300

    feat: add toast container and modify title and icon

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css'
 
 import GlobalStyles from 'styles/global'
 
@@ -7,9 +10,9 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>DevMarket</title>
+        <link rel="shortcut icon" href="/img/icon-192.png" />
+        <link rel="apple-touch-icon" href="/img/icon-192.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#06092B" />
         <meta
@@ -17,6 +20,7 @@ function App({ Component, pageProps }: AppProps) {
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
         />
       </Head>
+      <ToastContainer />
       <GlobalStyles />
       <Component {...pageProps} />
     </>
